4b66bbd7d10d25011253b611494d45df699da0e6
Terry Xu <knight3001@gmail.com>
2023-09-19 14:50:39 +1000

    set proxy true

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,7 @@ passport.deserializeUser((id, done) => {
   User.findById(id).then((user) => {
     done(null, user);
   });
-})
+});
 
 passport.use(
   new GoogleStrategy(
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {

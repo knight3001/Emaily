b0a35a94c4c4e9a21d92cd68a79c83f92c4549fc
Terry Xu <knight3001@gmail.com>
2023-09-26 14:30:14 +1000

    add router, redux sturecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => {
+  return <h2>Header</h2>;
+};
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>;
+};
+
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a href="/auth/google">Sign In With Google</a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/surveys" exact component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }

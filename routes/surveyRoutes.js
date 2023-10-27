a7d25d11ba83608c3c5ac016875c6ad30b1aae8e
Terry Xu <knight3001@gmail.com>
2023-10-27 14:48:58 +1000

    webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
+const _ = require("lodash");
 const moogoose = require("mongoose");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const MailgunMailer = require("../services/Mailer");
@@ -7,7 +10,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = moogoose.model("surveys");
 
 module.exports = (app) => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
@@ -37,5 +40,30 @@ module.exports = (app) => {
     } catch (err) {
       res.status(400).send(err);
     }
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    const data = req.body["event-data"];
+    const { recipient, url, event } = data;
+    const match = p.test(new URL(url).pathname);
+
+    if (match && event === "clicked") {
+      Survey.updateOne(
+        {
+          _id: match.surveyId,
+          recipients: {
+            $elemMatch: { email: recipient, responded: false },
+          },
+        },
+        {
+          $inc: { [match.choice]: 1 },
+          $set: { "recipients.$.responded": true },
+          lastResponded: new Date(),
+        }
+      ).exec();
+    }
+
+    res.send({});
   });
 };
